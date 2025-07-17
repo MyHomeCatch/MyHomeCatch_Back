0fddae353d5232a21223d81235cdc4f08b67a2d2
ahranah <window765@khu.ac.kr>
2025-07-17 16:06:49 +0900

    chore:initial setting

--- src/main/java/org/scoula/config/RootConfig.java
+++ src/main/java/org/scoula/config/RootConfig.java
@@ -9,19 +9,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
 @Configuration
 @PropertySource("classpath:application.properties")
-@MapperScan(basePackages = {"org.scoula.statics.mapper"})
-@ComponentScan(basePackages={ "org.scoula.statics.service"})
-@EnableTransactionManagement
+// @MapperScan(basePackages={})
 public class RootConfig {
     @Value("${jdbc.driver}") String driver;
     @Value("${jdbc.url}") String url;
