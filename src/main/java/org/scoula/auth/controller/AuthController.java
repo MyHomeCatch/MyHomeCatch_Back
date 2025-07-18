96b4a7c0c31821c661dcfa51c5c6ca464a369066
ryusemin <tpals3799@naver.com>
2025-07-18 15:05:30 +0900

    feat: 카카오 소셜 로그인 기능

--- src/main/java/org/scoula/auth/controller/AuthController.java
+++ src/main/java/org/scoula/auth/controller/AuthController.java
@@ -1,10 +1,12 @@
 package org.scoula.auth.controller;
 
 import org.scoula.auth.dto.AuthResponse;
+import org.scoula.auth.dto.KakaoLoginDto;
 import org.scoula.auth.dto.LoginRequest;
 import org.scoula.auth.dto.SignupRequest;
 import org.scoula.auth.service.AuthServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +27,20 @@ public class AuthController {
     @PostMapping("/signup")
     public AuthResponse signup(@RequestBody SignupRequest request) {
         return authServiceImpl.signup(request);
+    }
+
+    @PostMapping("/kakao")
+    public ResponseEntity<?> kakaoLogin(@RequestBody KakaoLoginDto kakaoLoginDto) {
+        String code = kakaoLoginDto.getCode();
+
+        AuthResponse authResponse = authServiceImpl.kakaoLogin(code);
+
+        return ResponseEntity.ok(authResponse);
+    }
+
+    @PostMapping("/kakaoSignup")
+    public ResponseEntity<?> kakaoSignup(@RequestBody KakaoLoginDto kakaoLoginDto) {
+
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/org/scoula/auth/service/AuthService.java
+++ src/main/java/org/scoula/auth/service/AuthService.java
@@ -4,8 +4,12 @@ import org.scoula.auth.dto.AuthResponse;
 import org.scoula.auth.dto.LoginRequest;
 import org.scoula.auth.dto.SignupRequest;
 
+import java.util.LinkedHashMap;
+
 public interface AuthService {
     AuthResponse login(LoginRequest request);
 
     AuthResponse signup(SignupRequest request);
+
+    AuthResponse kakaoLogin(String code);
 }
