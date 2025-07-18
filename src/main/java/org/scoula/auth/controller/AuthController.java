bfb9d5edb8651dc6db72fcc2c4d6ff5932830764
JH Kim <rlawogus65@gmail.com>
2025-07-18 11:33:58 +0900

    feat: 로그인 항목별 존재 유무 유효성 검사

--- src/main/java/org/scoula/auth/controller/AuthController.java
+++ src/main/java/org/scoula/auth/controller/AuthController.java
@@ -5,25 +5,56 @@ import org.scoula.auth.dto.LoginRequest;
 import org.scoula.auth.dto.SignupRequest;
 import org.scoula.auth.service.AuthServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
     @Autowired
-    private AuthServiceImpl authServiceImpl;
+    private AuthServiceImpl authService;
+
+//    @PostMapping("/login")
+//    public AuthResponse login(@RequestBody LoginRequest request) {
+//        return authServiceImpl.login(request);
+//    }
 
     @PostMapping("/login")
-    public AuthResponse login(@RequestBody LoginRequest request) {
-        return authServiceImpl.login(request);
+    public ResponseEntity<?> login(@Valid @RequestBody LoginRequest request, BindingResult result) {
+        if (result.hasErrors()) {
+            String message = result.getFieldErrors().stream()
+                    .map(FieldError::getDefaultMessage)
+                    .collect(Collectors.joining(", "));
+            return ResponseEntity.badRequest().body(message);
+        }
+
+        return ResponseEntity.ok(authService.login(request));
     }
 
-    @PostMapping("/signup")
-    public AuthResponse signup(@RequestBody SignupRequest request) {
-        return authServiceImpl.signup(request);
+//    @PostMapping("/signup")
+//    public AuthResponse signup(@RequestBody SignupRequest request) {
+//        return authServiceImpl.signup(request);
+//    }
+
+    @PostMapping(value = "/signup", produces = "text/pain;charset=UTF-8")
+    public ResponseEntity<?> signup(@Valid @RequestBody SignupRequest request, BindingResult result) {
+        if (result.hasErrors()) {
+            String message = result.getFieldErrors().stream()
+                    .map(FieldError::getDefaultMessage)
+                    .collect(Collectors.joining(", "));
+            return ResponseEntity.badRequest().body(message);
+        }
+
+        authService.signup(request);
+        return ResponseEntity.ok("회원가입 성공");
     }
 }
