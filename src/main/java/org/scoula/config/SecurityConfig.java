f1acf4ec311a96994bfb7d6f5ea9ff62c5605c47
JH Kim <rlawogus65@gmail.com>
2025-07-18 10:13:12 +0900

    feat: Security Config 작성

--- src/main/java/org/scoula/config/SecurityConfig.java
+++ src/main/java/org/scoula/config/SecurityConfig.java
@@ -1,5 +1,22 @@
 package org.scoula.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .anyRequest().permitAll(); // 일단 모든 경로 허용
+
+        return http.build();
+    }
 }
