5a49e3b96a5db349f472625a9f09861b3c69bc29
JH Kim <rlawogus65@gmail.com>
2025-07-18 09:03:10 +0900

    add: example.sql

---
+++ example.sql
@@ -0,0 +1,9 @@
+CREATE TABLE user (
+                      user_id INT AUTO_INCREMENT PRIMARY KEY COMMENT '사용자 ID',
+                      email VARCHAR(32) NOT NULL UNIQUE COMMENT '이메일',
+                      name VARCHAR(16) NOT NULL COMMENT '이름',
+                      nickname VARCHAR(16) NOT NULL COMMENT '닉네임',
+                      password VARCHAR(100) NOT NULL COMMENT '비밀번호',
+                      address VARCHAR(50) COMMENT '주소',
+                      additional_point INT DEFAULT NULL COMMENT '가점'
+)
