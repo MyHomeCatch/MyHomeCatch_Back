8911c3f12a0ca538b0b849692cd8b20c8cdafeb7
ryusemin <tpals3799@naver.com>
2025-07-21 15:27:13 +0900

    feat: googleLogin 로직 Controller => RestController로 변경

--- src/main/java/org/scoula/auth/dto/GoogleUserDto.java
+++ src/main/java/org/scoula/auth/dto/GoogleUserDto.java
@@ -6,6 +6,8 @@ import lombok.Data;
 @Data
 @Builder
 public class GoogleUserDto {
+    private String id;
+    private String token;
     private String email;
     private String name;
     // private String picture;
