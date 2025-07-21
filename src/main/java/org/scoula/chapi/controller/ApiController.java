3f3137293a4d3e99192f59da12a1a01dd875006e
Euangeliov <eva1europa@gmail.com>
2025-07-21 23:34:50 +0900

    feat: create GetMapping fetchData method

--- src/main/java/org/scoula/chapi/controller/ApiController.java
+++ src/main/java/org/scoula/chapi/controller/ApiController.java
@@ -2,9 +2,15 @@ package org.scoula.chapi.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.scoula.chapi.dto.CHOfficetelDTO;
 import org.scoula.chapi.service.DBService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Log4j2
 @RestController
@@ -12,4 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/ch/officetel")
 public class ApiController {
     private final DBService dbService;
+
+    @GetMapping("/fetchData")
+    public ResponseEntity<String> fetchData() {
+        log.info("Fetching officetel data");
+        try{
+            int affectedRows = dbService.fetchAndInsertAPIData();
+            String message = "Total "+affectedRows+" API data fetched successfully";
+            log.info(message);
+            return ResponseEntity.ok(message);
+        } catch (RuntimeException e) {
+            log.error("Error occured: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error occured: "+e.getMessage());
+        }
+    }
 }
