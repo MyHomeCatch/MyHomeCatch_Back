355bcd3646e7f9ce1eeabf002909bbfdf330e4c3
JH Kim <rlawogus65@gmail.com>
2025-07-21 10:24:56 +0900

    feat: 이메일, 닉네임 중복 확인 api 구현

--- src/main/java/org/scoula/auth/service/AuthService.java
+++ src/main/java/org/scoula/auth/service/AuthService.java
@@ -5,9 +5,14 @@ import org.scoula.auth.dto.LoginRequest;
 import org.scoula.auth.dto.SignupRequest;
 
 public interface AuthService {
+    boolean emailExists(String email);
+
+    boolean nicknameExists(String nickname);
+
     AuthResponse login(LoginRequest request);
 
     AuthResponse signup(SignupRequest request);
 
     void deleteByEmail(String email);
+
 }
