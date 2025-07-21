36580634351cf6bad21fb6664e5e72ff5fa304dd
Euangeliov <eva1europa@gmail.com>
2025-07-21 17:46:48 +0900

    feat: create DTO->VO and VO->DTO method in CHOfficetelDTO

--- src/main/java/org/scoula/chapi/service/DBService.java
+++ src/main/java/org/scoula/chapi/service/DBService.java
@@ -1,6 +1,12 @@
 package org.scoula.chapi.service;
 
-public interface DBService {
+import org.scoula.chapi.dto.CHOfficetelDTO;
 
+import java.util.List;
+
+public interface DBService {
+    CHOfficetelDTO insert(CHOfficetelDTO dto);
+
+    List<CHOfficetelDTO> insertAll(List<CHOfficetelDTO> dtoList);
 
 }
