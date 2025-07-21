577fecebc1382b51bf7f295db453a85c0b133916
Euangeliov <eva1europa@gmail.com>
2025-07-21 22:51:20 +0900

    feat: set RestTemplate Bean on RootConfig to enable restTemplate at DBServiceImpl

--- src/main/java/org/scoula/chapi/mapper/DBMapper.java
+++ src/main/java/org/scoula/chapi/mapper/DBMapper.java
@@ -1,7 +1,9 @@
 package org.scoula.chapi.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.scoula.chapi.domain.CHOfficetelVO;
 
+@Mapper
 public interface DBMapper {
     int insert(CHOfficetelVO vo);
 }
