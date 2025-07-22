0f0a41d45b775373c5e07b24fddaae3812bf810c
ryusemin <tpals3799@naver.com>
2025-07-22 16:39:40 +0900

    feat: 이메일 인증 기능

---
+++ src/main/java/org/scoula/email/service/MailServiceImpl.java
@@ -0,0 +1,59 @@
+package org.scoula.email.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+@Service
+@RequiredArgsConstructor
+public class MailServiceImpl implements MailService {
+
+    private final JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String senderEmail;
+
+    private static int number;
+
+    // 랜덤으로 숫자 생성
+    public static void createNumber() {
+        number = (int)(Math.random() * (90000)) + 100000; //(int) Math.random() * (최댓값-최소값+1) + 최소값
+    }
+
+    @Override
+    public MimeMessage CreateMail(String mail) {
+        createNumber();
+        MimeMessage message = javaMailSender.createMimeMessage();
+
+        try {
+            message.setFrom(new InternetAddress(senderEmail));
+            message.setRecipients(
+                    MimeMessage.RecipientType.TO,
+                    InternetAddress.parse(mail)
+            );
+            message.setSubject("이메일 인증");
+            String body = "";
+            body += "<h3>" + "요청하신 인증 번호입니다." + "</h3>";
+            body += "<h1>" + number + "</h1>";
+            body += "<h3>" + "감사합니다." + "</h3>";
+            message.setText(body,"UTF-8", "html");
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
+        return message;
+    }
+
+    @Override
+    public int sendMail(String mail) {
+        MimeMessage message = CreateMail(mail);
+        javaMailSender.send(message);
+
+        return number;
+    }
+}
