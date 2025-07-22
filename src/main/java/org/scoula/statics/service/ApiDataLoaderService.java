811be9c6525abdba67e1818c71247c12e71a5c06
rla_hyun <dldh0917@naver.com>
2025-07-22 16:31:28 +0900

    feat: 청약홈 공공데이터 수집 및 DB 저장 완료 #7

--- src/main/java/org/scoula/statics/service/ApiDataLoaderService.java
+++ src/main/java/org/scoula/statics/service/ApiDataLoaderService.java
@@ -8,13 +8,20 @@ import org.scoula.statics.domain.ApplicantRegionVO;
 import org.scoula.statics.domain.WinnerRegionVO;
 import org.scoula.statics.mapper.StaticsMapper;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Log4j2
 @Service
@@ -22,72 +29,81 @@ import java.nio.charset.StandardCharsets;
 public class ApiDataLoaderService {
 
     private final StaticsMapper mapper;
-    @Value("${api-key}")
+    @Value("${applyhome.api_key}")
     private String serviceKey;
 
     // 공공데이터 api -> DB 저장
-    public void applicantApiSave(String regionCode, int year) {
+    public void applicantApiSave(String regionCode, int year) throws IOException, URISyntaxException {
         System.out.println("call api");
         String apiUrl = "https://api.odcloud.kr/api/ApplyhomeStatSvc/v1/getAPTReqstAreaStat";
-        StringBuilder urlBuilder = new StringBuilder(apiUrl);
-        urlBuilder.append("?page=1&perPage=100");
-        urlBuilder.append("&cond[STAT_DE::LTE]=").append(Integer.toString(year+1));
-        urlBuilder.append("&cond[STAT_DE::GTE]=").append(Integer.toString(year));
-        urlBuilder.append("&serviceKey=").append(URLEncoder.encode(serviceKey, StandardCharsets.UTF_8));
-
-        System.out.println("------------");
-        System.out.println(urlBuilder.toString());
+        int page = 1, perPage = 100;
 
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.getForEntity(urlBuilder.toString(), String.class);
-        System.out.println("------------");
-        System.out.println(response.getStatusCode());
-        if (response.getStatusCode() == HttpStatus.OK) {
-            try {
-                ObjectMapper object = new ObjectMapper();
-                JsonNode dataNode = object.readTree(response.getBody()).get("data");
+        String url = UriComponentsBuilder.fromHttpUrl(apiUrl)
+                .queryParam("page", String.valueOf(page))
+                .queryParam("perPage", String.valueOf(perPage))
+                .queryParam("cond[STAT_DE::LTE]", String.valueOf(year+1))
+                .queryParam("cond[STAT_DE::GTE]", String.valueOf(year))
+                //.queryParam("serviceKey", serviceKey)
+                .toUriString();
 
-                if (dataNode != null && dataNode.isArray()) {
-                    for (JsonNode node : dataNode) {
-                        ApplicantRegionVO vo = applicantOf(node);
-                        mapper.insertApplicantRegion(vo);
-                    }
-                }
-            } catch (Exception e) {
-                log.error("API 응답 파싱 오류: " + e.getMessage());
-            }
+        url += "&serviceKey=" + serviceKey;
+        System.out.println(url);
+        URI uri = new URI(url);
+        //ResponseEntity<Map> resultMap = restTemplate.exchange(uri, HttpMethod.POST, null, Map.class);
+        String response = restTemplate.getForObject(uri, String.class);
+        //System.out.println(response);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode rootNode = objectMapper.readTree(response);
+        JsonNode dataArray = rootNode.path("data");
+
+        List<ApplicantRegionVO> applicantList = new ArrayList<>();
+
+        for (JsonNode node : dataArray) {
+            ApplicantRegionVO applicant = applicantOf(node);
+            applicantList.add(applicant);
         }
+
+        mapper.insertApplicantList(applicantList);
     }
 
-    public void winnerApiSave(String regionCode, int year) {
+    public void winnerApiSave(String regionCode, int year) throws URISyntaxException, IOException {
         String apiUrl = "https://api.odcloud.kr/api/ApplyhomeStatSvc/v1/getAPTPrzwnerAreaStat";
-        StringBuilder urlBuilder = new StringBuilder(apiUrl);
-        urlBuilder.append("?page=1&perPage=100");
-        urlBuilder.append("&cond[STAT_DE::GTE]=").append(year);
-        urlBuilder.append("&serviceKey=").append(serviceKey);
+        int page = 1, perPage = 100;
 
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.getForEntity(urlBuilder.toString(), String.class);
+        String url = UriComponentsBuilder.fromHttpUrl(apiUrl)
+                .queryParam("page", String.valueOf(page))
+                .queryParam("perPage", String.valueOf(perPage))
+                .queryParam("cond[STAT_DE::LTE]", String.valueOf(year+1))
+                .queryParam("cond[STAT_DE::GTE]", String.valueOf(year))
+                //.queryParam("serviceKey", serviceKey)
+                .toUriString();
 
-        if (response.getStatusCode() == HttpStatus.OK) {
-            try {
-                ObjectMapper object = new ObjectMapper();
-                JsonNode dataNode = object.readTree(response.getBody()).get("data");
+        url += "&serviceKey=" + serviceKey;
+        System.out.println(url);
+        URI uri = new URI(url);
+        //ResponseEntity<Map> resultMap = restTemplate.exchange(uri, HttpMethod.POST, null, Map.class);
+        String response = restTemplate.getForObject(uri, String.class);
+        //System.out.println(response);
 
-                if (dataNode != null && dataNode.isArray()) {
-                    for (JsonNode node : dataNode) {
-                        ApplicantRegionVO vo = applicantOf(node);
-                        mapper.insertApplicantRegion(vo);
-                    }
-                }
-            } catch (Exception e) {
-                log.error("API 응답 파싱 오류: " + e.getMessage());
-            }
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode rootNode = objectMapper.readTree(response);
+        JsonNode dataArray = rootNode.path("data");
+
+        List<WinnerRegionVO> winnerList = new ArrayList<>();
+
+        for (JsonNode node : dataArray) {
+            WinnerRegionVO winner = winnerOf(node);
+            winnerList.add(winner);
         }
+
+        mapper.insertWinnerList(winnerList);
     }
 
     private ApplicantRegionVO applicantOf(JsonNode node) {
-        long regionId = mapper.getRegionId(node.get("SUBSCRPT_AREA_CODE").asText());
+        long regionId = mapper.getRegionId(node.get("SUBSCRPT_AREA_CODE_NM").asText());
         return ApplicantRegionVO.builder()
                 .regionId(regionId)
                 .statDe(node.get("STAT_DE").asText())
@@ -99,7 +115,7 @@ public class ApiDataLoaderService {
     }
 
     private WinnerRegionVO winnerOf(JsonNode node) {
-        long regionId = mapper.getRegionId(node.get("SUBSCRPT_AREA_CODE").asText());
+        long regionId = mapper.getRegionId(node.get("SUBSCRPT_AREA_CODE_NM").asText());
         return WinnerRegionVO.builder()
                 .regionId(regionId)
                 .statDe(node.get("STAT_DE").asText())
