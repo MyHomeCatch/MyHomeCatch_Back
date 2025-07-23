7d6ffc8f4cb6d67494a86de5d53babc4cfab2c61
ryusemin <tpals3799@naver.com>
2025-07-23 09:34:30 +0900

    chore: emailController swagger 등록

--- src/main/java/org/scoula/email/controller/EmailController.java
+++ src/main/java/org/scoula/email/controller/EmailController.java
@@ -1,5 +1,10 @@
 package org.scoula.email.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.scoula.auth.dto.AuthResponse;
 import org.scoula.email.dto.EmailDTO;
 import org.scoula.email.dto.EmailRequestDto;
 import org.scoula.email.service.MailServiceImpl;
@@ -14,6 +19,7 @@ import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/api/email")
+@Api(tags = "이메일 인증 API")
 public class EmailController {
     private final MailServiceImpl mailServiceImpl;
     private int number; // 이메일 인증 숫자를 저장하는 변수
@@ -23,6 +29,12 @@ public class EmailController {
     }
 
     // 인증 이메일 전송
+    @ApiOperation(value = "이메일 인증번호 전송", notes = "인증번호를 이메일로 전송합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "성공적으로 요청이 처리되었습니다.", response = AuthResponse.class),
+            @ApiResponse(code = 401, message = "잘못된 요청입니다."),
+            @ApiResponse(code = 500, message = "서버에서 오류가 발생했습니다.")
+    })
     @PostMapping("/send")
     public ResponseEntity<EmailDTO> mailSend(@RequestBody EmailRequestDto emailRequestDto, HttpSession session) {
         EmailDTO emailDTO;
@@ -53,6 +65,12 @@ public class EmailController {
     }
 
     // 인증번호 일치여부 확인
+    @ApiOperation(value = "이메일 인증번호 검증", notes = "해당 이메일로 전송된 인증번호를 검증합니다")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "성공적으로 요청이 처리되었습니다.", response = AuthResponse.class),
+            @ApiResponse(code = 401, message = "잘못된 요청입니다."),
+            @ApiResponse(code = 500, message = "서버에서 오류가 발생했습니다.")
+    })
     @PostMapping("/check")
     public ResponseEntity<EmailDTO> mailCheck(
             @RequestBody EmailRequestDto requestDto,
