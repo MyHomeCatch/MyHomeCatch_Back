58d5b13159d68a7d4a5c776649179aad1a18394a
donggeon Na <ndmb2012@gmail.com>
2025-07-23 18:07:24 +0900

    test: 테스트 코드 삭제

--- src/main/java/org/scoula/lh/service/LhNoticeSchedulerService.java
+++ src/main/java/org/scoula/lh/service/LhNoticeSchedulerService.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
 public interface LhNoticeSchedulerService {
     List<NoticeDTO> getNotices();
     NoticeDTO getNotice(String panId);
