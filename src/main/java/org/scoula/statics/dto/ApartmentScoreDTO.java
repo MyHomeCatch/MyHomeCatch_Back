d070fc58c404e2e0fb0da4cc953b8156ed0aaa54
starwhale0w0 <starwhale@bu.ac.kr>
2025-07-23 09:26:09 +0900

    feat: api 연결

--- src/main/java/org/scoula/statics/dto/ApartmentScoreDTO.java
+++ src/main/java/org/scoula/statics/dto/ApartmentScoreDTO.java
@@ -1,17 +1,52 @@
 package org.scoula.statics.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+// 가점 하위 TOP5 조회 요청
+// 예시 : http://localhost:8080/api/low-popular/region-score?region=(지역 번호)
+// 지역번호 01, 02 등
 public class ApartmentScoreDTO {
-    private Long aptId;             // 아파트 고유 ID
-    private String aptName;         // 아파트 이름
-    private String regionCode;      // 지역 코드
-    private String address;         // 아파트 주소
-    private Integer averageScore;   // 평균 가점 부담 점수 (낮을수록 유리)
-    private Integer supplyCount;    // 공급 세대수
+    // 공고관리번호
+    @JsonProperty("HOUSE_MANAGE_NO")
+    private String houseManageNo;
+
+    // 공고번호
+    @JsonProperty("PBLANC_NO")
+    private String publicNoticeNo;
+
+    // 주택형 번호
+    @JsonProperty("MODEL_NO")
+    private String modelNo;
+
+    // 주택형
+    @JsonProperty("HOUSE_TY")
+    private String houseType;
+
+    // 거주지구분코드
+    @JsonProperty("RESIDE_SECD")
+    private String resideSecd;
+
+    // 거주지구분명
+    @JsonProperty("RESIDE_SENM")
+    private String resideSenm;
+
+    // 최저 당첨 점수
+    @JsonProperty("LWET_SCORE")
+    private String lowestScore;
+
+    // 최고 당첨 점수
+    @JsonProperty("TOP_SCORE")
+    private String topScore;
+
+    // 평균 당첨 점수
+    @JsonProperty("AVRG_SCORE")
+    private String averageScore;
 }
