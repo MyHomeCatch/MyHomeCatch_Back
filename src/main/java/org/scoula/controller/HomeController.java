31e552af85e518c59dd1e92cb38bbc93c9cf7cb7
ahranah <window765@khu.ac.kr>
2025-08-08 11:38:15 +0900

    docs: add swagger

--- src/main/java/org/scoula/controller/HomeController.java
+++ src/main/java/org/scoula/controller/HomeController.java
@@ -1,5 +1,7 @@
 package org.scoula.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Log4j2
+@Api(tags = "Home API")
 public class HomeController {
+    @GetMapping("/")
+    @ApiOperation(value = "Home", notes = "Returns the home page.")
     public String home() {
         log.info("================> HomController /");
         return "index"; // View 이름
